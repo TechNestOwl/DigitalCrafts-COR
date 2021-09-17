3da402141223fc10461747715a100cbb8259718e
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-09-17 09:48:48 -0400

    added more js

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -29,5 +29,17 @@ foo();
 
 const arry1 = ['a','b','c',4]
 
+// maping through an array:
+
+const numbers = [1,-1,-10,2,3];
+
+const filtered = numbers.filter(n => n >= 0);//filters out the negative numbers
+
+const listItemas = filtered.map(n => '<li>' + n + '</li>');
+
+//combines the itmes in the list. The empty ('') seperates the items with an empty character. As opposed to the defualt comma. 
+const html = '<ul>' + listItemas.join('') + '</ul>';
 
 
+
+console.log(html);
