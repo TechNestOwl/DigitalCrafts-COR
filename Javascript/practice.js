d1c943fe3485c2d431207f6e37fa71203ae558da
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-09-17 11:27:08 -0400

    added objects

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -76,3 +76,28 @@ console.log(y,x);
 // From the above example, we can see that primitive data types when passed 
 // to another variable, are passed by value. Instead of just assigning the same 
 // address to another variable, the value is passed and new space of memory is created.
+
+
+var myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+
+console.log(myCar);
+
+// four variables are created and assigned in a single go,
+// separated by commas
+var myObj = new Object(),
+    str = 'myString',
+    rand = Math.random(),
+    obj = new Object();
+
+myObj.type              = 'Dot syntax';
+myObj['date created']   = 'String with space';
+myObj[str]              = 'String value';
+myObj[rand]             = 'Random Number';
+myObj[obj]              = 'Object';
+myObj['']               = 'Even an empty string';
+
+console.log(myObj);
+
