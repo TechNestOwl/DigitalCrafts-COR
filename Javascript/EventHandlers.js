217e220fe8dfa43f4919db3763c04efaeddd12ec
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-09-23 10:28:22 -0400

    example 1

--- Javascript/EventHandlers.js
+++ Javascript/EventHandlers.js
@@ -5,11 +5,27 @@ The addEventListener() method attaches an event handler to an element without
 overwriting existing event handlers.
 
 EX:  document.getElementById("myBtn").addEventListener("click", displayDate);
-
 */
+
+
+
+// Change text to blue
 function blueFunction() {
-    document.getElementById("dyno").style.color = "blue";
-    alert("Text is now blue");
+    let x = document.getElementById("dyno");
+
+    x.style.color = "blue"
+    x.innerHTML="I am blue now";
+    alert("Success!");
+};
+
+// Change text to red
+function redFunction() {
+    let x = document.getElementById("dyno");
+
+    x.style.color = "red";
+    x.innerHTML="I am red now";
+    alert("Success!");
 };
 
 document.getElementById("blue-btn").addEventListener("click", blueFunction );
+document.getElementById("red-btn").addEventListener("click", redFunction );
