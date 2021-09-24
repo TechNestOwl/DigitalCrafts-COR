a3efa0a7482529dc69943a768bf00d33fa3d8327
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-09-24 10:39:16 -0400

    added js functionality

--- Javascript/shoppingList.js
+++ Javascript/shoppingList.js
@@ -1,4 +1,13 @@
 const btn = document.getElementById("add-btn");
 
-const input = document.getElementsByTagName("input");
+const input = document.getElementsByTagName("input")[0];
 console.log(input);
+
+btn.addEventListener("click", function(event){
+    // console.log(input.value);
+    let item = document.createElement("li")
+    item.innerHTML = input.value;
+
+    console.log(item);
+
+}); 
