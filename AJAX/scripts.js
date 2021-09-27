d52a80c80f82612cfde9bfda94236c66c2155e44
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-09-27 11:06:48 -0400

    added codes

--- AJAX/scripts.js
+++ AJAX/scripts.js
@@ -11,11 +11,32 @@
 //XHR
 const request = new XMLHttpRequest();
 
+request.onreadystatechange = function () {
+    console.log(this)
+};
+
+
+request.open("GET", "https://placekitten.com/400/400");
+
+const requestBTN = document.getElementById("requestBTN")
+requestBTN.addEventListener("click", function(){
+    request.send();
+});
+
 /**
  * Request METHODS
  *      -GET   : getting data from server
  *      -POST  : sending data to server
  *      -PATH  : sending UPDATED data/information
  *      -DELETE: deletes data 
+ */
+
+/**
+ * STATUS CODES
+ *  -100s Info
+ *  -200s Sucess
+ *  -300s Redirects
+ *  -400s Client Error
+ *  -500s Server Error
  */
 
