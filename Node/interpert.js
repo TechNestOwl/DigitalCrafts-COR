9a561246ff5cd20099d290b17eb82757f5e61553
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-10-11 10:35:06 -0400

    hosting examples

--- Node/interpert.js
+++ Node/interpert.js
@@ -1,2 +1,11 @@
 // console.log("hello world");
 const http = require("http");
+
+//which computer, where, are my files being hosted from?
+//this is like your house 
+const hostName = "127.0.0.1";
+
+//the exact place within the neighborhood that the files live 
+//think of this as a specific room inside your house
+
+const port ="3000";
