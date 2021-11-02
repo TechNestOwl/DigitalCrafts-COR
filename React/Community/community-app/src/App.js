0a371a8ee1267e7b801c10d2099fe398f1ccea1e
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-11-02 10:45:23 -0400

    Added react routing

--- React/Community/community-app/src/App.js
+++ React/Community/community-app/src/App.js
@@ -1,13 +1,43 @@
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      {/* <div className="App">
+        <header className="App-header">
+          Hello, React!
+        </header>
+      </div> */}
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/about">About</Link>
+
+            </li>
+            <li>
+            <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+            <Link to="/contacts">Contacts</Link>
+            </li>
+            <li>
+            <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
-export default App;
